0359bda561ebc479277c973780ec723dfbc7ddd8
dehadeaaryan <dehadeaaryan@gmail.com>
2024-04-18 12:17:07 -0500

    Created Pricing view

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,99 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from 'primevue/menu';
+import Button from 'primevue/button';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+        <div id="app-name">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+            <h1>SuperFrog Scheduler</h1>
+        </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <Button icon="pi pi-user" text raised rounded aria-label="User" />
+            </nav>
+        </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+}
+
+#app-name {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    width: 100px;
+    height: auto;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    gap: 1rem;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+    margin-left: 1rem;
+    text-decoration: none;
+    text-align: center;
+    color: var(--color-text);
+    border-radius: 0.25rem;
+    transition: background-color 0.3s;
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
 }
 
 nav a:first-of-type {
-  border: 0;
+    border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+        padding: 1rem 0;
+        margin-top: 1rem;
+    }
 }
 </style>
---
+++ src/views/PricingView.vue
@@ -0,0 +1,65 @@
+<template>
+    <main>
+        <h1>Pricing</h1>
+        <section class="pricing-info">
+            <h2>Terms & Conditions</h2>
+            <ul>
+                <li>ALL Requests must be submitted at least 14 days prior to event date, and no earlier than 3 months out. The site will not permit requests outside these parameters.</li>
+                <li>All requests must be submitted through the online request system. E-mailed requests are not considered.</li>
+                <li>Submission of a request does NOT guarantee acceptance. TCU Spirit has until 5 days prior the date of the event to accept or decline a request.</li>
+                <li>Prices are based on a 1 hour appearance – Additional fees may apply if more than one hour.</li>
+                <li>ALL appearances are paid. We do not offer any free appearances.</li>
+            </ul>
+
+            <h2>Spirit Appearance Fees</h2>
+            <!-- Add Spirit Appearance Fees here -->
+        </section>
+
+        <section class="general-terms">
+            <h2>General Terms:</h2>
+            <ul>
+                <li>Requests must be submitted at least 14 days prior to event date.</li>
+                <li>TCU has until 5 days before the date of the appearance to accept or decline.</li>
+                <li>Prices are based on a 1 hour event. Additional fees may apply if more than one hour.</li>
+                <li>Appearances are only available in the DFW area.</li>
+                <li>The SuperFrog suit will not be rented out to a third party.</li>
+                <li>Due to university rules, TCU Spirit will not make appearances during dead (study) days, finals week, or major holidays including, but not limited to, New Years Eve and Day, MLK Day, 4th of July, Thanksgiving Day, Christmas Eve or Day.</li>
+            </ul>
+        </section>
+
+        <section class="contact-terms">
+            <h2>Contact Terms:</h2>
+            <p>Unless otherwise specified, the contact provided and their information will also serve as our Spirit Squad member’s point-of-contact on the date of the event.</p>
+            <p>List the address and all other necessary information on the booking site.</p>
+        </section>
+
+        <section class="event-terms">
+            <h2>Event terms:</h2>
+            <ul>
+                <li>Submission of a request does not guarantee the event will be accepted. TCU Spirit has until 5 days prior the date of the event to accept or decline a request.</li>
+                <li>Even after an event is confirmed, it is still subjected to immediate cancellation due to a TCU Athletic Department function, weather or an emergency. Immediate notice will be given in the event of cancellation.</li>
+                <li>Serious consideration is taken to each request to ensure it deems a positive reflection of TCU Spirit and the University.</li>
+                <li>Superfrog and TCU Spirit members are approved for University social media content only and not available to request for any other content.</li>
+            </ul>
+        </section>
+
+        <section class="transportation-terms">
+            <h2>Transportation Terms:</h2>
+            <ul>
+                <li>Must include mileage if location is more than 2 miles from TCU and include any parking fees/expenses.</li>
+                <li>If more than 2 miles from TCU, a $0.75 per mile transportation fee will be included in the appearance total.</li>
+                <li>6+ Spirit Squad members will require transportation to be booked to and from Fort Worth and paid for by requestor.</li>
+            </ul>
+        </section>
+
+        <section class="payment-terms">
+            <h2>Payment Terms:</h2>
+            <ul>
+                <li>All appearances are paid. We do not offer volunteer appearances.</li>
+                <li>Payment should be submitted a minimum of 24 hours prior to event or it will be cancelled.</li>
+                <li>You will be given a link for payment via credit card upon approval of your appearance through email.</li>
+                <li>TCU Department Appearances are required to pay via a Fund Transfer Form: <a href="https://vcfa.tcu.edu/accounting/">https://vcfa.tcu.edu/accounting/</a>. The completed form and all required budget codes must be emailed to the individuals included in the appearance approval e-mail to finalize request.</li>
+            </ul>
+        </section>
+    </main>
+</template>
